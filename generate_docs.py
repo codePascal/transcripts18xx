7b73e5928be8c672bb66b2e5aad5caf530d0c331
Pascal <mup0117@gmail.com>
2025-04-09 18:43:31 +0200

    Add update step before building docs.

--- generate_docs.py
+++ generate_docs.py
@@ -4,6 +4,17 @@ import subprocess
 
 
 def main():
+    update()
+    build()
+
+
+def update():
+    subprocess.run(
+        ['sphinx-apidoc', '-o', './docs/source', './transcripts18xx']
+    )
+
+
+def build():
     subprocess.run(
         ['sphinx-build', '-M', 'html', './docs/source', './docs/build'],
         check=True
