4743b40d44f1427e9118b375a93ad946f7c19b77
Pascal <mup0117@gmail.com>
2025-04-10 13:45:34 +0200

    Test for transcript added.

---
+++ tests/test_transcript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+from transcripts18xx import transcript, games
+
+from tests import context
+
+
+class TestModuleTranscript(unittest.TestCase):
+
+    def test_parse_G1830(self):
+        # TODO: test
+        df = transcript.parse(context.transcript_1830(), games.Game1830())
